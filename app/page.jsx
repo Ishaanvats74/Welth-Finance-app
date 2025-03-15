101d878ac502326f066102c29d6a6c38a012209d
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-15 22:59:30 +0530

    time stamp 40:00

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button variant='destructive'>Button</Button>
+    <div className="mt-40"> 
+      <Button variant='destructive'>Button</Button>
+    </div>
   );
 }
