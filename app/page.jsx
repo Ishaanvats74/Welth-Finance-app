dbcbf91d93530559f6f4524b272e9f121563c42b
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-15 16:46:15 +0530

    clerk added

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
-    <>Hello
-    </>
+    <Button variant='destructive'>Button</Button>
   );
 }
