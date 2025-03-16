0065ba6856eeea4742132691fd1dbe84e1604178
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-17 01:15:18 +0530

    completed till 52:00 having problem in gloabal css

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,13 @@
 'use client';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
 const HeroSection = () => {
   return (
     <div className='pb-20 px-4 mt-40'>
-        <div>
-            <h1>
+        <div className='container mx-auto text-center'>
+            <h1 className='text-3xl md:text5xl lg:text-[96px] pb-6 gradient-title'>
                 Manage your Finance <br /> with Intelligence
             </h1>
             <p>An AI-powered financial management platform that helps you track, analyze, and optimize your spending with real-time insights.</p>
@@ -17,6 +18,11 @@ const HeroSection = () => {
                 <Link href="/">
                     <button size='lg' className='px-7 rounded-md py-2 shadow hover:bg-white/80 hover:text-black/80'> Learn More</button>
                 </Link>
+            </div>
+            <div>
+                <div>
+                    <Image src={"/banner.jpeg"} width={1280} height={720} alt='Dashboard Preview' priority className='rounded-lg shadow-2xl border mx-auto'></Image>
+                </div>
             </div>
         </div>
     </div>
